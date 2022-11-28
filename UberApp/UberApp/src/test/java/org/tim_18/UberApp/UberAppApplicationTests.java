931e36dd3e850c4bc96d9d4af752b4f6cf4c143a
Anja Petković <anjapetkovic92@gmail.com>
2022-11-28 16:25:55 +0100

    Initial commit
    [Spring boot app] [initialized]

---
+++ UberApp/UberApp/src/test/java/org/tim_18/UberApp/UberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tim_18.UberApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
