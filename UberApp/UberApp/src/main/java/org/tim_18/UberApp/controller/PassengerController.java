fa50776f864f205f949fc21f5f6501a50673e12d
Kristina Andrijin <andrijinkristina@gmail.com>
2022-12-07 10:46:53 +0100

    get and post passenger request, findAllDTO

--- UberApp/UberApp/src/main/java/org/tim_18/UberApp/controller/PassengerController.java
+++ UberApp/UberApp/src/main/java/org/tim_18/UberApp/controller/PassengerController.java
@@ -1,64 +1,66 @@
 package org.tim_18.UberApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.tim_18.UberApp.dto.PassengerDTO;
-import org.tim_18.UberApp.mapper.PassengerDTOMapper;
-import org.tim_18.UberApp.model.Location;
+import org.tim_18.UberApp.dto.FindAllDTO;
+import org.tim_18.UberApp.dto.passengerDTOs.PassengerDTOnoPassword;
+import org.tim_18.UberApp.dto.passengerDTOs.PassengerDTOwithPassword;
+import org.tim_18.UberApp.dto.UserActivationDTO;
+import org.tim_18.UberApp.mapper.passengerDTOmappers.PassengerDTOnoPasswordMapper;
+import org.tim_18.UberApp.mapper.passengerDTOmappers.PassengerDTOwithPasswordMapper;
 import org.tim_18.UberApp.model.Passenger;
-import org.tim_18.UberApp.model.Ride;
 import org.tim_18.UberApp.service.PassengerService;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 @RestController
-@RequestMapping("/passenger")
+@RequestMapping("/api/passenger")
 public class PassengerController {
     private final PassengerService passengerService;
+    @Autowired
+    private PassengerDTOwithPasswordMapper dtoWithPasswordMapper;
+    @Autowired
+    private PassengerDTOnoPasswordMapper dtoNoPasswordMapper;
 
     public PassengerController(PassengerService passengerService) {
         this.passengerService = passengerService;
     }
 
-    @GetMapping()
-    public ResponseEntity<List<Passenger>> findAll() {
-        List<Passenger> passengers = passengerService.findAll();
-        List<PassengerDTO> passengersDTO = new ArrayList<>() ;
-        for (Passenger p : passengers) {
-            System.out.println(p);
-            passengersDTO.add(new PassengerDTO(p));
-        }
-        return new ResponseEntity<>(passengers, HttpStatus.OK);
+    @PostMapping()
+    public ResponseEntity<PassengerDTOnoPassword> addPassenger(@RequestBody PassengerDTOwithPassword userActivationDTO) {
+        PassengerDTOwithPassword dto = passengerService.save(userActivationDTO);
+        Passenger passenger = dtoWithPasswordMapper.fromDTOtoPassenger(dto);
+        PassengerDTOnoPassword retDto = dtoNoPasswordMapper.fromPassengerToDTO(passenger);
+        return new ResponseEntity<>(retDto, HttpStatus.CREATED);
     }
 
-    @GetMapping("/find{id}")
+    @GetMapping()
+    public ResponseEntity<FindAllDTO<PassengerDTOnoPassword>> findAll() {
+        List<Passenger> passengers = passengerService.findAll();
+        List<PassengerDTOnoPassword> passengersDTO = new ArrayList<>() ;
+        for (Passenger p : passengers) {
+            passengersDTO.add(new PassengerDTOnoPassword(p));
+        }
+        FindAllDTO<PassengerDTOnoPassword> retDTO = new FindAllDTO<PassengerDTOnoPassword>(passengersDTO);
+        return new ResponseEntity<>(retDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("activate/{activationId}")
+    public ResponseEntity<UserActivationDTO> activateUser(){
+        return new ResponseEntity<>(new UserActivationDTO(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
     public ResponseEntity<Passenger> findById(@PathVariable("id") Integer id) {
         Passenger passenger = passengerService.findById(id);
         return new ResponseEntity<>(passenger, HttpStatus.OK);
     }
 
-    @PostMapping()
-    public ResponseEntity<PassengerDTO> addPassenger(@RequestBody PassengerDTO userActivationDTO) {
-        Passenger passenger = new Passenger();
-        passenger.setName(userActivationDTO.getName());
-        passenger.setSurname(userActivationDTO.getSurname());
-        passenger.setAddress(userActivationDTO.getAddress());
-        passenger.setEmail(userActivationDTO.getEmail());
-        passenger.setPassword(userActivationDTO.getPassword());
-        passenger.setImageLink(userActivationDTO.getImageLink());
-        passenger.setTelephoneNumber(userActivationDTO.getTelephoneNumber());
-        PassengerDTO retDTO = new PassengerDTO(passenger);
-        HashSet<Location> locs = new HashSet<>();
-        HashSet<Ride> rides = new HashSet<>();
-        passenger.setFavouriteLocations(locs);
-//        passenger.setRides(rides);
-        return new ResponseEntity<>(retDTO, HttpStatus.CREATED);
-    }
 
-    @PutMapping("/update")
+    @PutMapping("/{id}/ride")
     public ResponseEntity<Passenger> updatePassenger(@RequestBody Passenger passenger) {
         Passenger updatedPassenger = passengerService.updatePassenger(passenger);
         return new ResponseEntity<>(updatedPassenger, HttpStatus.OK);
--- UberApp/UberApp/src/main/java/org/tim_18/UberApp/service/PassengerService.java
+++ UberApp/UberApp/src/main/java/org/tim_18/UberApp/service/PassengerService.java
@@ -1,23 +1,26 @@
 package org.tim_18.UberApp.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.tim_18.UberApp.dto.passengerDTOs.PassengerDTOwithPassword;
 import org.tim_18.UberApp.exception.UserNotFoundException;
-import org.tim_18.UberApp.mapper.PassengerDTOMapper;
-import org.tim_18.UberApp.model.Location;
+import org.tim_18.UberApp.mapper.passengerDTOmappers.PassengerDTOwithPasswordMapper;
 import org.tim_18.UberApp.model.Passenger;
-import org.tim_18.UberApp.model.Ride;
 import org.tim_18.UberApp.repository.PassengerRepository;
 
-import java.util.HashSet;
 import java.util.List;
 
 @Service("passengerService")
 public class PassengerService {
     @Autowired
     private final PassengerRepository passengerRepository;
+    @Autowired
+    ModelMapper modelMapper;
+    @Autowired
+    PassengerDTOwithPasswordMapper dtoMapper;
 
     public PassengerService(PassengerRepository passengerRepository) {
         this.passengerRepository = passengerRepository;
@@ -27,23 +30,23 @@ public class PassengerService {
         return passengerRepository.save(passenger);
     }
 
-    public Page<Passenger> findAll(Pageable page){
-        return passengerRepository.findAll(page);
-    }
-
-    public List<Passenger> findAll(){
+    public List<Passenger> findAll() {
         return passengerRepository.findAll();
     }
 
     public Passenger updatePassenger(Passenger passenger) {
         return passengerRepository.save(passenger);
-    };
-
-    public Passenger findById(Integer id) {
-        return passengerRepository.findPassengerById(id).orElseThrow(() -> new UserNotFoundException("Passenger by id " + id + " was not found") );
     }
 
-    public Passenger save(Passenger passenger){
-        return passengerRepository.save(passenger);
+    ;
+
+    public Passenger findById(Integer id) {
+        return passengerRepository.findPassengerById(id).orElseThrow(() -> new UserNotFoundException("Passenger by id " + id + " was not found"));
+    }
+
+    public PassengerDTOwithPassword save(PassengerDTOwithPassword dto) {
+        Passenger passenger = dtoMapper.fromDTOtoPassenger(dto);
+        passenger = passengerRepository.save(passenger);
+        return dtoMapper.fromPassengerToDTO(passenger);
     }
 }
