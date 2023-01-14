4ff509352ec2f254f778d72e8e2a06d63a844c6b
Anja Petković <anjapetkovic92@gmail.com>
2023-01-14 17:16:25 +0100

    Updated endpoints for review

--- UberApp/UberApp/src/main/java/org/tim_18/UberApp/repository/ReviewRepository.java
+++ UberApp/UberApp/src/main/java/org/tim_18/UberApp/repository/ReviewRepository.java
@@ -11,11 +11,11 @@ import java.util.HashSet;
 
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-//    public Page<Review> findAll(Pageable pageable);
+    //    public Page<Review> findAll(Pageable pageable);
     @Query(value = "SELECT * FROM reviews rev NATURAL JOIN passenger_rides  NATURAL JOIN rides rid NATURAL JOIN drivers driv  where driv.vehicle_id = ?1 and rev.is_driver = false", nativeQuery = true)
     Page<Review> findByVehicleId(int id, Pageable pageable);
 
-//    rev NATURAL JOIN rides rid NATURAL JOIN passenger p where rev.passenger_id = p.id and rid.id = ?1
+    //    rev NATURAL JOIN rides rid NATURAL JOIN passenger p where rev.passenger_id = p.id and rid.id = ?1
     @Query(value = "SELECT * FROM reviews rev where rev.ride_id = ?1 ", nativeQuery = true)
     HashSet<Review> findByRideIdHash(Integer id);
 
