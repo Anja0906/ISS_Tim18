fdae8a1756a1ac04c7332ce8adf64dc679e9c5db
Anja Petković <anjapetkovic92@gmail.com>
2023-01-25 15:56:25 +0100

    Clean code in ReviewController
    Clean code in ReviewController

--- UberApp/UberApp/src/main/java/org/tim_18/UberApp/controller/ReviewController.java
+++ UberApp/UberApp/src/main/java/org/tim_18/UberApp/controller/ReviewController.java
@@ -10,6 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.tim_18.UberApp.Validation.ErrorMessage;
 import org.tim_18.UberApp.dto.*;
+import org.tim_18.UberApp.dto.passengerDTOs.PassengerEmailDTO;
 import org.tim_18.UberApp.dto.passengerDTOs.PassengerIdEmailDTO;
 import org.tim_18.UberApp.exception.DriverNotFoundException;
 import org.tim_18.UberApp.exception.PassengerNotFoundException;
@@ -45,20 +46,19 @@ public class ReviewController {
         this.vehicleService     = vehicleService;
         this.passengerService   = passengerService;
         this.driverService      = driverService;
-        this.userService = userService;
+        this.userService        = userService;
     }
 
     @PreAuthorize("hasRole('PASSENGER')")
     @PostMapping("/{rideId}/vehicle")
-    public ResponseEntity<?> addReviewToVehicle(Principal principal,
-                                                @PathVariable("rideId") int rideId,
-                                                @RequestBody ReviewPostDTO reviewPostDTO) {
+    public ResponseEntity<?> addReviewToVehicle(
+            Principal principal,
+            @PathVariable("rideId") int rideId,
+            @RequestBody ReviewPostDTO reviewPostDTO) {
         try {
             Ride ride = rideService.findRideById(rideId);
-
             checkPassengersAuthorities(principal, ride);
             Passenger passenger = passengerService.findById(userService.findUserByEmail(principal.getName()).getId());
-
             Review review = reviewService.findByRideAndPassengerIdForVehicle(ride.getId(), passenger.getId());
             if (!(review==null)) {
                 return new ResponseEntity<>(new ErrorMessage("Review for vehicle already left!"), HttpStatus.BAD_REQUEST);
@@ -71,9 +71,7 @@ public class ReviewController {
             reviews.add(review);
             ride.setReviews(new HashSet<>(reviews));
             rideService.updateRide(ride);
-
-            ReviewDTO reviewDTO = new ReviewDTO(review);
-            return new ResponseEntity<>(reviewDTO, HttpStatus.OK);
+            return new ResponseEntity<>(new ReviewDTO(review), HttpStatus.OK);
         }catch(RideNotFoundException e){
             return new ResponseEntity<>("Ride does not exist!", HttpStatus.NOT_FOUND);
         }catch (VehicleNotFoundException e){
@@ -84,18 +82,16 @@ public class ReviewController {
 
     @PreAuthorize("hasAnyRole('ADMIN', 'PASSENGER')")
     @GetMapping("/vehicle/{id}")
-    public ResponseEntity<?> getReviewsForVehicle (Principal principal,
-                                                   @PathVariable("id") int id,
-                                                   @RequestParam(defaultValue = "0") Integer page,
-                                                   @RequestParam(defaultValue = "4") Integer size) {
+    public ResponseEntity<?> getReviewsForVehicle (
+            Principal principal,
+            @PathVariable("id") int id,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "4") Integer size) {
         try{
             Vehicle vehicle = vehicleService.findVehicleById(id);
             Pageable pageable = PageRequest.of(page, size);
             Page<Review> reviews;
-            int role = checkRole(principal);
-            // ako zahtev salje putnik, vracamo mu reviews koje je on ostavio
-            // adminu vracamo sve reviews
-            if (role == 1){
+            if (checkRole(principal) == 1){
                 Integer passengerId = userService.findUserByEmail(principal.getName()).getId();
                 reviews = reviewService.findByVehicleAndPassengerId(id, passengerId, pageable);
             } else {
@@ -117,9 +113,10 @@ public class ReviewController {
 
     @PreAuthorize("hasRole('PASSENGER')")
     @PostMapping("/{rideId}/driver")
-    public ResponseEntity<?> addReviewToDriver(Principal principal,
-                                               @PathVariable("rideId") int rideId,
-                                               @RequestBody ReviewPostDTO reviewPostDTO) {
+    public ResponseEntity<?> addReviewToDriver(
+            Principal principal,
+            @PathVariable("rideId") int rideId,
+            @RequestBody ReviewPostDTO reviewPostDTO) {
         try {
             Ride ride = rideService.findRideById(rideId);
 
@@ -148,18 +145,16 @@ public class ReviewController {
 
     @PreAuthorize("hasAnyRole('ADMIN', 'PASSENGER')")
     @GetMapping("/driver/{id}")
-    public ResponseEntity<?> getReviewsForDriver (Principal principal,
-                                                  @PathVariable("id") int id,
-                                                  @RequestParam(defaultValue = "0") Integer page,
-                                                  @RequestParam(defaultValue = "4") Integer size) {
+    public ResponseEntity<?> getReviewsForDriver (
+            Principal principal,
+            @PathVariable("id") int id,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "4") Integer size) {
         try{
             Driver driver = driverService.findDriverById(id);
             Pageable pageable = PageRequest.of(page, size);
             Page<Review> reviews;
-            int role = checkRole(principal);
-            // ako zahtev salje putnik, vracamo mu reviews koje je on ostavio
-            // adminu vracamo sve reviews
-            if (role == 1){
+            if (checkRole(principal) == 1){
                 Integer passengerId = userService.findUserByEmail(principal.getName()).getId();
                 reviews = reviewService.findByDriverAndPassengerId(id, passengerId, pageable);
             } else {
@@ -178,39 +173,20 @@ public class ReviewController {
         }
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{rideId}")
     public ResponseEntity<?> getReviewsForRide (
             @PathVariable("rideId") int id) {
         try {
             Ride ride = rideService.findRideById(id);
-            HashSet<Review> reviews = reviewService.findByRideIdHash(id);
+            Set<Passenger> passengers = ride.getPassengers();
             HashSet<ReviewDTOResponse> reviewsDTO = new HashSet<>();
-            Passenger passenger;
-            PassengerIdEmailDTO passengerEmailDTO;
-            int ind = 0;
-            for (Review review : reviews) {
-                passenger = passengerService.findById(review.getPassenger().getId());
-                passengerEmailDTO = new PassengerIdEmailDTO(passenger);
-                if (review.getDriver() == true) {
-                    for (Review reviewVehicle : reviews)
-                        if (reviewVehicle.getPassenger().getId() == review.getPassenger().getId() && reviewVehicle.getDriver() == false) {
-                            reviewsDTO.add(new ReviewDTOResponse(new VehicleReviewDTO(reviewVehicle, passengerEmailDTO), new DriverReviewDTO(review, passengerEmailDTO)));
-                            ind = 1;
-                        }
-                    if (ind == 0)
-                        reviewsDTO.add(new ReviewDTOResponse(new VehicleReviewDTO(), new DriverReviewDTO(review, passengerEmailDTO)));
-
-                } else {
-                    for (Review reviewDriver : reviews)
-                        if (reviewDriver.getPassenger().getId() == review.getPassenger().getId() && reviewDriver.getDriver() == true) {
-                            reviewsDTO.add(new ReviewDTOResponse(new VehicleReviewDTO(review, passengerEmailDTO), new DriverReviewDTO(reviewDriver, passengerEmailDTO)));
-                            ind = 1;
-                        }
-                    if (ind == 0)
-                        reviewsDTO.add(new ReviewDTOResponse(new VehicleReviewDTO(review, passengerEmailDTO), new DriverReviewDTO()));
-                }
-                ind = 0;
+            Review reviewDriver,reviewVehicle;
+            for (Passenger passenger:passengers) {
+                reviewDriver = reviewService.findByRideAndPassengerIdForDriver(id,passenger.getId());
+                reviewVehicle = reviewService.findByRideAndPassengerIdForVehicle(id,passenger.getId());
+                reviewsDTO.add(new ReviewDTOResponse(
+                        new VehicleReviewDTO(reviewVehicle,new PassengerIdEmailDTO(passenger)),
+                        new DriverReviewDTO(reviewDriver,new PassengerIdEmailDTO(passenger))));
             }
             return new ResponseEntity<>(reviewsDTO, HttpStatus.OK);
         } catch (RideNotFoundException e) {
@@ -233,13 +209,11 @@ public class ReviewController {
     private int checkRole(Principal principal) {
         User user = userService.findUserByEmail(principal.getName());
         Integer userId = user.getId();
-        boolean isAdmin = false;
         boolean isPassenger = false;
         while (true) {
             try {
-                if (!isAdmin) {
+                if (!isPassenger) {
                     Passenger passenger = passengerService.findById(userId);
-//                    isPassenger = true;
                     return 1;
                 }
             }
@@ -250,9 +224,7 @@ public class ReviewController {
     }
 
     private void checkAuthorities(Principal principal, Ride ride) throws RideNotFoundException {
-        int role = checkRole(principal);
-        if (role == 1){
+        if (checkRole(principal) == 1)
             checkPassengersAuthorities(principal, ride);
-        }
     }
 }
